b54711b76193302754b05413b4dc8bf4183650f2
Adrianjpa <112101037+audiovisualjpedu@users.noreply.github.com>
2023-02-27 20:28:08 -0300

    Primeiro commit do projeto Mais Digital 2

---
+++ assets/js/scrollpages.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+  // anima a rolagem da página quando um link do menu é clicado
+  $('a[href^="#"]').on('click', function(event) {
+    event.preventDefault();
+    var target = $(this.hash);
+    if (target.length) {
+      $('html, body').animate({
+        scrollTop: target.offset().top
+      }, 1000);
+    }
+  });
+});
