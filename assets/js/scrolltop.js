b6cbf6807f3c9fd438794021024f40161b368dd1
audiovisualjpedu <112101037+audiovisualjpedu@users.noreply.github.com>
2023-02-28 11:07:56 -0300

    Tentativas de mudanças
    Algumas tentativas de mudanças foram implementadas, como o carousel, mas não teve exito

--- assets/js/scrolltop.js
+++ assets/js/scrolltop.js
@@ -8,7 +8,7 @@ $(document).ready(function() {
   });
 
   $('#btn-top').click(function() {
-    $('html, body').animate({scrollTop : 0},800);
+    $('html, body').animate({scrollTop : 0},400);
     return false;
   });
 });
