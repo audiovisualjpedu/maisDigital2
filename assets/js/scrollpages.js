2604ffcb0cae1bb240d30ce619ff37354ca770a4
audiovisualjpedu <112101037+audiovisualjpedu@users.noreply.github.com>
2023-03-10 14:38:31 -0300

    Imagens deletadas e reorganização do codigo

--- assets/js/scrollpages.js
+++ assets/js/scrollpages.js
@@ -1,6 +1,5 @@
 $(document).ready(function() {
-  // anima a rolagem da página quando um link do menu é clicado
-  $('a[href^="#"]').on('click', function(event) {
+   $('a[href^="#"]').on('click', function(event) {
     event.preventDefault();
     var target = $(this.hash);
     if (target.length) {
